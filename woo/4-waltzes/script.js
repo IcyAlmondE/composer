6b26eb3eb9162c770b17b592aa6eadd18c11329a
IcyAlmondE <natthapol2549@gmail.com>
2024-12-24 00:26:08 +0700

    Add some url gimmicks

--- woo/4-waltzes/script.js
+++ woo/4-waltzes/script.js
@@ -1,4 +1,12 @@
 function scrollToTopic(topicId) {
-    const topic = document.getElementById(topicId);
+  const topic = document.getElementById(topicId);
+  if(topicId==='return'){
     topic.scrollIntoView({ behavior: "smooth" });
+    history.pushState(null, null, ` `);
+    return;
   }
+  if (topic) {
+    topic.scrollIntoView({ behavior: "smooth" });
+    history.pushState(null, null, `#${topicId}`);
+  }
+}
