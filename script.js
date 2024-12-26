43f24415be2ff6a52616f2a8d804eb8575ccc965
IcyAlmondE <natthapol2549@gmail.com>
2024-12-26 12:08:59 +0700

    Table function is now working

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function createTableForYear(year) {
   const data = {
     "2015":[
-        {"c":"1", "composition":"Piece in G minor", "nickname":"", "date":"2015", "note":"", "link":""}
+        {"c":"1", "composition":"Piece in G minor", "nickname":"-", "date":"2015", "note":"-", "link":""}
     ],
     "2020":[
-        {"c":"1a", "composition":"Variations on a theme by \"Loong Rerng\"", "nickname":"", "date":"2020", "note":"", "link":""},
-        {"c":"2", "composition":"Piece in G-flat major", "nickname":"Autumn Breeze", "date":"November 2020", "note":"", "link":""},
-        {"c":"3", "composition":"Nocturne in E major", "nickname":"", "date":"December 2020", "note":""},
-        {"c":"4", "composition":"Waltz in A-flat major", "nickname":"new year Waltz", "date":"December 2020", "note":"", "link":""}
+        {"c":"1a", "composition":"Variations on a theme by \"Loong Rerng\"", "nickname":"-", "date":"2020", "note":"-", "link":""},
+        {"c":"2", "composition":"Piece in G-flat major", "nickname":"Autumn Breeze", "date":"November 2020", "note":"-", "link":""},
+        {"c":"3", "composition":"Nocturne in E major", "nickname":"-", "date":"December 2020", "note":"-"},
+        {"c":"4", "composition":"Waltz in A-flat major", "nickname":"New year Waltz", "date":"December 2020", "note":"-", "link":""}
     ],
     "2021":[
         {"c":"5", "composition":"", "nickname":"", "date":"2020", "note":"", "link":""},
@@ -24,26 +24,33 @@ function createTableForYear(year) {
         {"c":"16", "composition":"", "nickname":"", "date":"2020", "note":"", "link":""}
     ]
   };
-  let tablehtml = '<table><thead><tr><th>C.</th><th>Composition</th><th>Nickname</th><th>Date</th><th>Note</th></tr></thead></table>';
+  let tablehtml = '<table><thead><tr><th>C.</th><th>Composition</th><th>Nickname</th><th>Date</th><th>Note</th></tr></thead>';
   tablehtml+='<tbody>';
   data[year].forEach(piece => {
-    if (piece[link]){
-      tablehtml += `<tr><a src=\"piece[link]\"><td>'${piece.c}'</td> <td>'${piece.composition}'</td> <td>'${piece.nickname}'</td> <td>'${piece.date}'</td> <td>'${piece.note}'</td></a></tr> |`
+    if (piece.link){
+      tablehtml += 
+      `<tr>
+        <a src=${piece.link}>
+          <td>${piece.c}</td>
+          <td>${piece.composition}</td>
+          <td>${piece.nickname}</td>
+          <td>${piece.date}</td>
+          <td>${piece.note}</td>
+        </a>
+      </tr>`;
     }
     else{
-      tablehtml += `<tr><td>'${piece.c}'</td> <td>'${piece.composition}'</td> <td>'${piece.nickname}'</td> <td>'${piece.date}'</td> <td>'${piece.note}'</td> </tr> |`;
+      tablehtml +=
+      `<tr>
+        <td>${piece.c}</td>
+        <td>${piece.composition}</td>
+        <td>${piece.nickname}</td>
+        <td>${piece.date}</td>
+        <td>${piece.note}</td>
+      </tr>`;
     }
   });
   tablehtml+='</tbody></table>';
 
   return tablehtml;
-}
-  
-function loadTableForYear() {
-  const containers = document.querySelectorAll(".table-container");
-
-  containers.forEach(container => {
-      const year = container.getAttribute("data-year"); // Get the year from the data attribute
-      container.innerHTML = createTableForYear(year); // Generate and insert the table
-  })
 }
